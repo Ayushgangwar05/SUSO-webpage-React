45b3c637794f9753355f980e797f043d1920c411
Ayush Gangwar <ayush.ganwar@gmail.com>
2023-10-02 22:27:46 +0530

    Your comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Allcomponents/Navbar";
+import "./App.css";
+import "./Allcomponents/Allcsapp.css";
+import SEOSupport from "./Allcomponents/SEOSupport";
+import Search1 from "../src/Allcomponents/Search1";
+import Superiority from "./Allcomponents/Superiority";
+import Partners from "./Allcomponents/Partners";
+import Cardposition from "./Allcomponents/Cardposition";
+import CaseStudies from "./Allcomponents/CaseStudies";
+import Blog from "../src/Allcomponents/Blog";
+import Blog1 from "./Allcomponents/Blog1";
+import Footer from "./Allcomponents/Footer";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div >
+        <SEOSupport />
+        <Search1 />
+        <Superiority />
+        <Partners />
+        <Cardposition />
+        <CaseStudies />
+        <Blog />
+        <Blog1 />
+        <Footer />
+      </div>
     </div>
   );
 }
